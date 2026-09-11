9a6f8ef64ba9270bede5983667100745b3b927c9
root <root@localhost.localdomain>
2026-09-12 06:36:29 +1300

    fix: restaurar datos de panaderia en configuracion

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -1,30 +1,58 @@
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class SettingsService {
-  static const String boxName = 'settings';
+  static const String _boxName = 'settings';
 
   static const String bakeryNameKey = 'bakery_name';
+  static const String bakeryAddressKey = 'bakery_address';
+  static const String bakeryPhoneKey = 'bakery_phone';
+  static const String bakeryOwnerKey = 'bakery_owner';
   static const String currencyKey = 'currency';
 
-  late Box _box;
-
-  Future<void> init() async {
-    if (!Hive.isBoxOpen(boxName)) {
-      _box = await Hive.openBox(boxName);
-    } else {
-      _box = Hive.box(boxName);
-    }
-  }
+  Box get _box => Hive.box(_boxName);
 
   String get bakeryName {
     return _box.get(
       bakeryNameKey,
+      defaultValue: 'Panadería y Pastelería Carballog FP',
+    ) as String;
+  }
+
+  String get bakeryAddress {
+    return _box.get(
+      bakeryAddressKey,
       defaultValue: '',
     ) as String;
   }
 
-  Future<void> saveBakeryName(String value) async {
-    await _box.put(bakeryNameKey, value);
+  String get bakeryPhone {
+    return _box.get(
+      bakeryPhoneKey,
+      defaultValue: '',
+    ) as String;
+  }
+
+  String get bakeryOwner {
+    return _box.get(
+      bakeryOwnerKey,
+      defaultValue: '',
+    ) as String;
+  }
+
+  Future<void> saveBakeryName(String name) async {
+    await _box.put(bakeryNameKey, name);
+  }
+
+  Future<void> saveBakeryAddress(String address) async {
+    await _box.put(bakeryAddressKey, address);
+  }
+
+  Future<void> saveBakeryPhone(String phone) async {
+    await _box.put(bakeryPhoneKey, phone);
+  }
+
+  Future<void> saveBakeryOwner(String owner) async {
+    await _box.put(bakeryOwnerKey, owner);
   }
 
   String get currency {
